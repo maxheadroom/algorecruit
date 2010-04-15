09f41023edc1d7710134fd0fef8c16932f182e55
Falko Zurell <falko.zurell@gmail.com>
2010-04-15 23:25:59 +0200

    added more github API calls for user information

--- src/githubapi.rb
+++ src/githubapi.rb
@@ -17,11 +17,9 @@ class GithubAPI
     @token = token
   end
   
-  
+  # fetch userinformation about user
   def get_userinfo (user)
     # /user/show/:username
-    verb = "get"
-    # method = ('Net::HTTP::' + verb.to_s.capitalize).to_class
     url = BASE_URL + BASE_FORMAT + "/user/show/#{user}"
     uri = URI.parse url
     server = Net::HTTP.new(uri.host, uri.port)
@@ -37,6 +35,39 @@ class GithubAPI
     
   end
   
+  
+  # fetch the list of followings for user
+  def get_followings (user)
+    # /user/show/:user/following
+    url = BASE_URL + BASE_FORMAT + "/user/show/#{user}/following"
+    uri = URI.parse url
+    server = Net::HTTP.new(uri.host, uri.port)
+    server.use_ssl = (uri.scheme == 'https')
+    server.verify_mode = OpenSSL::SSL::VERIFY_NONE if server.use_ssl?
+    response = server.start do |http|
+      req = Net::HTTP::Get.new(uri.path)
+      # req.form_data = data.merge(auth)
+      http.request(req)
+    end
+    JSON.parse(response.body)
+  end
+
+  # fetch the list of followers to user
+  def get_followers (user)
+    # /user/show/:user/followers
+    url = BASE_URL + BASE_FORMAT + "/user/show/#{user}/followers"
+    uri = URI.parse url
+    server = Net::HTTP.new(uri.host, uri.port)
+    server.use_ssl = (uri.scheme == 'https')
+    server.verify_mode = OpenSSL::SSL::VERIFY_NONE if server.use_ssl?
+    response = server.start do |http|
+      req = Net::HTTP::Get.new(uri.path)
+      # req.form_data = data.merge(auth)
+      http.request(req)
+    end
+    JSON.parse(response.body)
+  end
+
   
   #end class
 end
--- src/test.rb
+++ src/test.rb
@@ -4,14 +4,28 @@ require 'GithubAPI'
 require 'yaml'
 
 config = YAML.load_file("authconfig.yml")
-user_name = config["user_name"]
-token = config["token"]
 
-github = GithubAPI.new(user_name, token)
+
+github = GithubAPI.new(config["user_name"], config["token"])
 
 myuser = github.get_userinfo("mattb")
+
 myuser.each_key { |k|
   printf("%s : %s\n", k, myuser[k])
   }
 
-printf("%s", myuser.class)
+followings = github.get_followings("mattb")['users']
+
+printf("\n=========== Following count: %i ===========\n\n", followings.length)
+
+followings.each { |k|
+  printf("Following: %s \n", k)
+  }
+  
+  
+followers = github.get_followers("mattb")['users']
+
+printf("\n=========== Follower count: %i ===========\n\n", followers.length)
+followers.each { |k|
+    printf("Follower: %s \n", k)
+    }
